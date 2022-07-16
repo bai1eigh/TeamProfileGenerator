4608fd71067836123eaa702da3e30c52b22d194d
Baileigh <bai1eigh@outlook.com>
2022-07-16 19:06:29 -0400

    added to library, javascript for engineer to pass tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var inquirer = require('inquirer');
 const fs = require('fs');
-
+const generateMarkdown = require('./assets/js/script.js');
 
 // TODO: Create an array of questions for user input // Creating an array of objects: https://www.freecodecamp.org/news/javascript-array-of-objects-tutorial-how-to-create-update-and-loop-through-objects-using-js-array-methods/
 const questions = [
@@ -37,7 +37,7 @@ message:'Please enter your email address.',
 
 },
 
-  //menu question
+  //menu question (when prompted this question it should be redirected to one or the other)
   {
 
     name: 'Menu',
@@ -82,7 +82,7 @@ message:'Please enter engineers email address.',
 
 },
 
-  //menu question
+  //menu question (when prompted this question it should be redirected to one or the other)
   {
 
     name: 'Menu',
@@ -126,37 +126,25 @@ message:'Please enter interns email address.',
   message: 'Please tell us the school the intern attends.',
 
 },
-
-  //menu question
-  {
-
-    name: 'Menu',
-    type:'list',
-    message: 'Would you like to add either an engineer or intern?',
-    choices:  
-    ['Engineer', 
-    'Intern',],
-},
 ];
 
 /*end of intern */
 
 
 // TODO: Create a function to write index file
-function writeToFile(fileName, data) {
-return fs.writeFileSync(fileName, data) ;
-}
-
+function build() {
+  fs.writeFileSync('myTeam.html', gt(teamManager, teamEngineer, teamIntern));
+  console.log("myTeam.html file succesfully created!");
 // TODO: Create a function to initialize app
 function init() {
 
-  inquirer.prompt(questions).then((data) => {
-  //    console.log(JSON.stringify(data, null, " "));
-     // data.getLicense = getLicense(data.license);
-     writeToFile('./index.html', generateMarkdown(data));
-  });
+inquirer.prompt(questions).then((data) => {
+ //    console.log(JSON.stringify(data, null, " "));
+    // data.getLicense = getLicense(data.license);
+    writeToFile('./index.html', script(data));
+});
 }
 
 // Function call to initialize app
 init();
-
+};
