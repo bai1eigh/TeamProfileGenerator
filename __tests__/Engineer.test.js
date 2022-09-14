68a4fc5b6fcf0d17075f38e994a79d2c5bb397ff
Baileigh <brownbaileigh1@gmail.com>
2022-09-14 15:17:01 -0400

    adding tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer");
+
+test("Can create a github.", () => {
+    const testGithub = "PhilsFuture";
+    const employeeInstance = new Engineer("Phil", 7, "Philfuture@gmail.com", testGithub);
+    expect(employeeInstance.github).toBe(testGithub);
+});
+
+test("Testing getGithub will return github.", () => {
+    const testGithub = "JamesLJenks";
+    const employeeInstance = new Engineer("Keely", 2, "Keely@gmail.com", testGithub);
+    expect(employeeInstance.getGithub()).toBe(testGithub);
+});
+
+test("Testing role.", () => {
+    const returnValue = "Engineer";
+    const employeeInstance = new Engineer("Raven", 2, "RavenB@gmail.com", "RavenB");
+    expect(employeeInstance.getRole()).toBe(returnValue);
+});
